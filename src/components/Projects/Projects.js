ad54d771797d9a187abd05c513830850a7abdf5b
Patrick Neff <pbneff@yahoo.com>
2019-04-25 21:32:39 -0600

    got each project in line and mapping

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,7 @@ import "./Projects.css";
 const ProjectList = [{
     HomeUrl: "https://pad-holder.herokuapp.com/",
     Name: "Virtual Pad Holder",
-    Image: "",
+    Image: "images/pad-screenshot.png",
     GitHub: "https://github.com/discopat1/Pad-holder-app",
     Description1: "Web application for Boxers, Kickboxers, MMA fighters, or anyone looking to improve their reaction time while doing bag-work or shadow boxing. Includes a round timer and uses speech synthesis to call out classic fight combinations during the rounds!",
     Description2: "Built with ReactJS, MongoDB, NodeJS, Express, and Bootstrap."
@@ -25,15 +25,16 @@ class Projects extends Component {
 
 renderProjectList = project => (
     <React.Fragment>
-        <a href={project.HomeUrl}><p>{project.Name}</p>
-            <img src={project.Image} class="img-responsive" alt=""/>
-            <div>
-            <div>
-                <p>{project.Description1}</p>
-                <p>{project.Description2}</p>
-            </div>
-            </div>
-        </a>
+        <div class="col-sm-6 work">
+            <p>{project.Name}</p>
+            <a href={project.HomeUrl}><img src={project.Image} class="img-responsive img-work" alt=""/></a>
+                <div>
+                <div>
+                    <p>{project.Description1}</p>
+                    <p>{project.Description2}</p>
+                </div>
+                </div>
+        </div>
 
     </React.Fragment>
 )
@@ -43,7 +44,7 @@ renderProjectList = project => (
             <div class="container-fluid bg-3 text-center">
                 <h3>A snapshot of some of my past and current projects</h3>
                 <div class="row">
-                    <div class="col-sm-5 work">
+                    
                         {/* <a href="https://workoutappsrevenge.herokuapp.com/"><p>The Workout App</p>
                         <img src={'images/Workoutapp-screenshot.png'} class="img-responsive img-work" alt=""/>
                             <div class="overlay">
@@ -55,7 +56,7 @@ renderProjectList = project => (
                         </a>
                         <a href="https://github.com/smitty10010/workoutappsrevenge"><p>Just the code</p></a> */}
                         {ProjectList.map(this.renderProjectList)}
-                    </div>
+                    
                 </div>
             </div>
         );
