02db17984b5764a3fcc122a26fdda6406b952fae
Patrick Neff <pbneff@yahoo.com>
2019-04-25 18:36:29 -0600

    sanity check

---
+++ src/components/Projects/ProjectList.js
@@ -0,0 +1,10 @@
+const ProjectList = {
+    HomeUrl: "https://pad-holder.herokuapp.com/",
+    Name: "Virtual Pad Holder",
+    Image: "",
+    GitHub: "https://github.com/discopat1/Pad-holder-app",
+    Description1: "Web application for Boxers, Kickboxers, MMA fighters, or anyone looking to improve their reaction time while doing bag-work or shadow boxing. Includes a round timer and uses speech synthesis to call out classic fight combinations during the rounds!",
+    Description2: "Built with ReactJS, MongoDB, NodeJS, Express, and Bootstrap."
+}
+
+export default {ProjectList};
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import ProjectList from './ProjectList';
 
 import "./Projects.css";
 
 class Projects extends Component {
     render() {
         return (
-            <div>
-                Stuff
+            <div class="container-fluid bg-3 text-center">
+                <h3>A snapshot of some of my past and current projects</h3>
+                <div class="row">
+                    <div class="col-sm-3">
+                       <h3>{ProjectList.Name}</h3>
+                    </div>
+                </div>
             </div>
         );
       }
