868315b5f3a6025dd86f04789768e9e3670a00aa
Patrick Neff <pbneff@yahoo.com>
2019-04-30 17:22:30 -0600

    ditched overlay and switched to cards

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -32,57 +32,34 @@ const ProjectList = [{
     Name: "The Workout App",
     Image: "images/Workoutapp-screenshot.png",
     GitHub: "https://github.com/smitty10010/workoutappsrevenge",
-    Description1: "Web application designed to provide a strength and conditioning workout based on user input. Exercises, weight, sets, reps, plus history tracking is provided.",
-    Description2: "Built with ReactJS, Material UI, Auth0, Node.JS, Express, Lodash, and Passport."
+    Description1: "Test.",
+    Description2: "Test."
 },
 ];
 
 
 class Projects extends Component {
 
-    constructor(){
-        super();
-
-        this.state = {
-           overlay: false
-        }
-    }
-
-    overLayOn(){
-       this.setState({overlay: true})
-    }
-
-    overLayOff() {
-        this.setState({overlay: false})
-    }
-
-    toggle() {
-        return this.state.overlay ? 'block' : 'none';
-    }
 
     renderProjectList = project => (
         <React.Fragment>
             <div class="col-sm-6 work">
-                <p>{project.Name}</p>
-                <a href={project.HomeUrl}><img src={project.Image} class="img-responsive img-work" alt=""/></a>
-                    <div class="container info-button">
-                        <button class="primary" onClick={this.overLayOn.bind(this)}>Info</button>
-                    </div>
-                    <div className="overlay" style={{display: this.toggle()}} onClick={this.overLayOff.bind(this)}>
-                        <div className="over-text">
+                <div class="card" style={{width: '75%'}}>
+                <img src={project.Image} class="img-responsive img-work" alt=""/>
+                    <div class="card-body">
+                        <h5 class="card-title">{project.Name}</h5>
                             <p>{project.Description1}</p>
                             <p>{project.Description2}</p>
-                        </div>
+                            <a href={project.HomeUrl} class="btn btn-outline-secondary">Application</a>
+                            <a href={project.GitHub} class="btn btn-outline-secondary">Repo</a>
                     </div>
-                    
+                </div>
             </div>
-
         </React.Fragment>
     )
 
 
     render() {
-        console.log(this.state.overlay)
         return (
             <div class="container-fluid bg-3 text-center">
                 <h3>A snapshot of some of my past and current projects</h3>
