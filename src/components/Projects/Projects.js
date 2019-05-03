06c8164946de06bb8d443262c62e6fb3bf473740
Patrick Neff <pbneff@yahoo.com>
2019-05-02 20:29:27 -0600

    projects in good shape minus some polishing

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,7 @@ import "./Projects.css";
 const ProjectList = [{
     HomeUrl: "https://pad-holder.herokuapp.com/",
     Name: "Virtual Pad Holder",
-    Image: "images/pad-screenshot.png",
+    Image: "images/pad-react.png",
     GitHub: "https://github.com/discopat1/Pad-holder-app",
     Description1: "Web application for Boxers, Kickboxers, MMA fighters, or anyone looking to improve their reaction time while doing bag-work or shadow boxing. Includes a round timer and uses speech synthesis to call out classic fight combinations during the rounds!",
     Description2: "Built with ReactJS, MongoDB, NodeJS, Express, and Bootstrap."
@@ -14,7 +14,7 @@ const ProjectList = [{
 {
     HomeUrl: "https://workoutappsrevenge.herokuapp.com/",
     Name: "The Workout App",
-    Image: "images/Workoutapp-screenshot.png",
+    Image: "images/workout-react.png",
     GitHub: "https://github.com/smitty10010/workoutappsrevenge",
     Description1: "Web application designed to provide a strength and conditioning workout based on user input. Exercises, weight, sets, reps, plus history tracking is provided.",
     Description2: "Built with ReactJS, Material UI, Auth0, Node.JS, Express, Lodash, and Passport."
@@ -22,7 +22,7 @@ const ProjectList = [{
 {
     HomeUrl: "https://smitty10010.github.io/team-5-project1/",
     Name: "Crag",
-    Image: "images/crag2.png",
+    Image: "images/crag-react.png",
     GitHub: "https://github.com/smitty10010/team-5-project1",
     Description1: "Web application built to find rock climbing areas based on experience and goals. Group project for users interested in reducing the barrier of entry to the rock climbing community.",
     Description2: "Built with HTML, JQuery, Materialize, Google Map API, OpenWeatherMap API, Mountain Project API, and Meetup API."
@@ -30,7 +30,7 @@ const ProjectList = [{
 {
     HomeUrl: "https://blooming-island-62700.herokuapp.com/",
     Name: "GOT Friend Finder",
-    Image: "images/got-screenshot.png",
+    Image: "images/got-react.png",
     GitHub: "https://github.com/discopat1/Friend-finder-express",
     Description1: "Friend matching web application. User is first given the opportunity to download a picture of themselves and is then asked to answer some survey questions. The application will then pair the user with their best matched Game of Thrones character based on their answers.",
     Description2: "Built with HTML, NodeJS, Express, and Bootstrap."
@@ -52,8 +52,8 @@ class Projects extends Component {
                         <p>{project.Description2}</p>
                     </div>     
                     <div class="d-flex justify-content-between card-footer">
-                        <a href={project.HomeUrl} class="btn btn-outline-secondary">Application</a>
-                        <a href={project.GitHub} class="btn btn-outline-secondary" style={{width: '22%'}}>Repo</a>
+                        <a href={project.HomeUrl} class="btn btn-outline-secondary card-button">Application</a>
+                        <a href={project.GitHub} class="btn btn-outline-secondary card-button">Repo</a>
                     </div>
                 </div>
             </div>
@@ -65,7 +65,7 @@ class Projects extends Component {
         return (
             <div class="container-fluid bg-3 text-center">
                 <h3>A snapshot of some of my past and current projects</h3>
-                <div class="card-group">
+                <div class="card-group" style={ {backgroundImage: "url('images/sunset.jpg')"} }>
                     {ProjectList.map(this.renderProjectList)}
                 </div>
             </div>
